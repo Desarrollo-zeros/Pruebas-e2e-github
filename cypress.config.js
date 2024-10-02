31a22dfb26ee2a77be44816b4659857e14a139de
carlos castilla <ccastill@bancolombia.com.co>
2024-10-01 19:05:28 -0500

    add allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,20 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const cucumber = require('cypress-cucumber-preprocessor').default;
+
+
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      on('file:preprocessor', cucumber());
+      return config;
     },
     video: true,
     screenshotOnRunFailure: true
   },
+  env: {
+    allure: true
+  }
 });

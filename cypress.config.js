ed82e2cfd1613a2889a93f476f268014baa4057c
carlos castilla <ccastill@bancolombia.com.co>
2024-10-01 19:37:00 -0500

    .

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,6 @@
 const { defineConfig } = require("cypress");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
-
-
-
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
